47d77ae4c3753d214a2aa912ca9fb15905392cc2
houseofmoo <moodie2john@gmail.com>
2017-08-15 16:18:08 -0700

    c# code

---
+++ CSharp/Common/ValueConverters/BaseValueConverter.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Markup;
+
+namespace Common.ValueConverters
+{
+    /// <summary>
+    /// A base value converter that allows direct XAML usage.
+    /// </summary>
+    /// <typeparam name="T">The type of this value converter.</typeparam>
+    public abstract class BaseValueConverter<T> : MarkupExtension, IValueConverter
+        where T : class, new()
+    {
+        #region private fields
+        /// <summary>
+        /// A single static instance of this value converter.
+        /// </summary>
+        private static T _converter = null;
+        #endregion
+
+        #region markup extension methods
+        /// <summary>
+        /// Provides a static instnace of the value converter.
+        /// </summary>
+        /// <param name="serviceProvider">The service provider.</param>
+        /// <returns></returns>
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            if (_converter == null)
+                _converter = new T();
+
+            return _converter;
+        } 
+        #endregion
+
+        #region value converter methods
+        /// <summary>
+        /// The method to convert one type to another.
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="targetType"></param>
+        /// <param name="parameter"></param>
+        /// <param name="culture"></param>
+        /// <returns></returns>
+        public abstract object Convert(object value, Type targetType, object parameter, CultureInfo culture);
+
+        /// <summary>
+        /// The method to convert a value back to it's source type.
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="targetType"></param>
+        /// <param name="parameter"></param>
+        /// <param name="culture"></param>
+        /// <returns></returns>
+        public abstract object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture);
+        #endregion
+    }
+}
---
+++ CSharp/Common/ViewModels/BaseViewModel.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+
+namespace Common.ViewModels
+{
+    /// <summary>
+    /// Base ViewModel other view models inherit from to get OnPropertyChanged event.
+    /// </summary>
+    public class BaseViewModel : INotifyPropertyChanged
+    {
+        private PropertyChangedEventHandler _propertyChanged;
+        event PropertyChangedEventHandler INotifyPropertyChanged.PropertyChanged
+        {
+            add { this._propertyChanged += value; }
+            remove { _propertyChanged -= value; }
+        }
+
+        /// <summary>
+        /// When a property's value is updated, this even fires
+        /// </summary>
+        /// <param name="propertyName">Name of property that has been updated</param>
+        private void OnPropertyChanged(string propertyName)
+        {
+            _propertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        /// <summary>
+        /// Used to set the value of a property
+        /// </summary>
+        /// <typeparam name="T">Values type</typeparam>
+        /// <param name="property">Property to be updated</param>
+        /// <param name="newValue">Value property will be updated to</param>
+        /// <param name="propertyName">Name of property to be updated</param>
+        public void SetProperty<T>(ref T property, T newValue, string propertyName)
+        {
+            // if current and new value are equal do nothing
+            if (EqualityComparer<T>.Default.Equals(property, newValue))
+                return;
+
+            // set current to new value
+            property = newValue;
+
+            // invoke property changed event
+            this.OnPropertyChanged(propertyName);
+        }
+    }
+}
